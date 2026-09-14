85d7f969612f1d7e9fd016d9cfcddb527fe32aef
amuthaangamuthu72-sketch <amuthaangamuthu72@gmail.com>
2026-09-14 15:32:12 +0530

    Update server port for Render

--- server.js
+++ server.js
@@ -41,7 +41,7 @@ app.get("/", (req, res) => {
 });
 
 // Start server
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`MediKey Backend running on http://localhost:${PORT}`);
